f80422d6621ed5dade9d06bd4bdbf3ebc18f4f67
Justin Carter <axis@berkeley.edu>
2015-04-18 03:13:45 -0300

    Remove outdated spec files.

--- spec/controllers/email_notifications_controller_spec.rb
+++ spec/controllers/email_notifications_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe EmailNotificationsController do
+describe EmailNotificationsController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # EmailNotification. As you add validations to EmailNotification, be sure to
