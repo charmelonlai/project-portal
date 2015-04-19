8e20fb87424d88f3808c9bffafecdd1d17ad066b
Charles Lai <charles.lai@berkeley.edu>
2015-04-19 04:10:15 -0700

    Delete questions.rb

--- spec/factories/questions.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :question do
-    question "MyString"
-    subject "MyString"
-    input_type "MyString"
-  end
-end
