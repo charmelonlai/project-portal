8fba533526b896e8a517409db69b608205bc7757
Justin Carter <axis@berkeley.edu>
2015-05-01 00:26:40 -0300

    Remove favorites-related code.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,2 @@
 module ProjectsHelper
-	def isFav(id)
-		project = Project.find(id)
-	    proj = Favorite.where("project_id = ? AND user_id = ?", project.id, current_user).limit(1)
-	    if proj.empty?
-	      return false
-	    else
-	      return true
-	    end
-	end
 end
