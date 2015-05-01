d156f709735863d09e0c7f4d0d9bb25e7ce57219
Justin Carter <axis@berkeley.edu>
2015-05-01 07:30:26 -0300

    Add tests for Organization model.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Organization do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '#sname_hash_to_org_list' do
+    def create_org_with_sname(sname)
+      FactoryGirl.create(:organization, :sname => sname)
+    end
+
+    before(:each) do
+      Organization.delete_all
+      
+      @cs169 = create_org_with_sname('CS169')
+      @bp = create_org_with_sname('BP')
+      @cs160 = create_org_with_sname('CS160')
+      @bc = create_org_with_sname('BC')
+    end
+  
+    it 'returns all the organizations whose sname has a value of 1 in the hash' do
+      got = Organization.sname_hash_to_org_list({ 'CS169' => '1', 'BP' => '1', 'CS160' => 0 })
+      expect(got).to match_array([@cs169, @bp])
+    end
+    
+    it 'returns an empty collection if no sname has a value of 1 in the hash' do
+      got = Organization.sname_hash_to_org_list({ 'CS169' => '0', 'BP' => '0', 'BC' => '0' })
+      expect(got).to be_empty
+    end
+  end
+  
 end
